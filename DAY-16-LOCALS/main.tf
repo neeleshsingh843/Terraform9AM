cd8e2883955849259b843a899d8ee5056e6041c1
NEELESH SINGH <neeleshgec843@gmail.com>
2024-09-06 18:45:00 +0530

    DAY-16-LOCALS UPDATES

---
+++ DAY-16-LOCALS/main.tf
@@ -0,0 +1,15 @@
+locals {
+  region = "ap-south-1"
+  environment = "dev"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a4408457f9a03be3" # Amazon Linux 2 AMI
+  instance_type = local.instance_type
+
+  tags = {
+    Name        = "ExampleInstance"
+    Environment = local.environment
+  }
+}
